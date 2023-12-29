c967d1f75e88ff02731584e9d1c55350f26152a3
xlh <1121079556@qq.com>
2023-12-29 20:37:09 +0800

    add context router

--- main.go
+++ main.go
@@ -1,25 +1,26 @@
 package main
 
 import (
-	"fmt"
 	"gee_web/gee"
-	"log"
 	"net/http"
 )
 
 func main() {
-	gee := gee.New()
-	gee.GET("/", indexHandle)
-	gee.POST("/hello", helloHandle)
-	log.Fatal(gee.Run(":9999"))
-}
+	r := gee.New()
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	})
+	r.GET("/hello", func(c *gee.Context) {
+		// expect /hello?name=geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+	})
 
-func indexHandle(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "URL.path=%q\n", req.URL.Path)
-}
+	r.POST("/login", func(c *gee.Context) {
+		c.JSON(http.StatusOK, gee.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
+	})
 
-func helloHandle(w http.ResponseWriter, req *http.Request) {
-	for k, v := range req.Header {
-		fmt.Fprintf(w, "head[%q]=%q\n", k, v)
-	}
+	r.Run(":9999")
 }
